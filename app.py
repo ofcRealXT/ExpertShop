0546e08ff5495a1cc4fe78bd1229bdd234a75e45
ofcRealXT <talhaozc.2010@gmail.com>
2025-04-19 13:02:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import os
 load_dotenv()
 
 app= Flask(__name__)
-app.secret_key= os.getenv()
-app.config["SQLALCHEMY_DATABASE_URI"]= os.getenv()
+app.secret_key= os.getenv('SECRET_KEY')
+app.config["SQLALCHEMY_DATABASE_URI"]= os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 app.config['UPLOAD_FOLDER']= 'static/uploads'
 
