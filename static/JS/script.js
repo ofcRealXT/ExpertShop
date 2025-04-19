421c0cf17c49e3e2d2ca7835d8f13bacb8d6c2a4
ofcRealXT <talhaozc.2010@gmail.com>
2025-04-19 20:34:36 +0300

    Create script.js

---
+++ static/JS/script.js
@@ -0,0 +1,18 @@
+function actionSelect() {
+    let option= document.getElementById("action");
+    let command= document.getElementById("command");
+  
+    if (option.value=== "banuser") {
+        command.type= "number";
+        command.placeholder= "User id";
+    } else if (option.value=== "deleteproduct") {
+        command.type= "number";
+        command.placeholder= "Product id";
+    } else if (option.value== "deletecomment") {
+        command.type= "number";
+        command.placeholder= "Comment id";
+    } else {
+        command.type= "text";
+        command.placeholder= "Products, comments, users";
+    }
+  }
