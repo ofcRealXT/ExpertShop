94ca16cefe111e5d5de84cec22d60a5a7db496ed
ofcRealXT <talhaozc.2010@gmail.com>
2025-04-20 13:42:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -522,8 +522,5 @@ def logout():
     session.pop('user_id', None)
     return redirect(url_for('home'))
 
-with app.app_context():
-    db.create_all()
-
 if __name__ == "__main__":
     app.run(debug=True)
