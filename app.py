8e3295afc61b4129e62d267396f5ece4501b02b2
ofcRealXT <talhaozc.2010@gmail.com>
2025-05-18 16:53:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -643,6 +643,9 @@ def admin():
     
     return render_template("admin.html", users=users, products=products, comments=comments)
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
     app.run(debug=True)
     #app.run(host="0.0.0.0", port=5000, debug=True) # Aynı ip'deki cihazların girebilmesi için
